283b1a4bc2f37749e2e3fab80757427629f200cc
jsugarman <joel.sugarman@digital.justice.gov.uk>
2015-10-27 22:51:07 +0000

    add admin superuser and include question model on admin

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Question
+# from .models import Choice
+
+# admin.site.register(Question)
+# admin.site.register(Choice)
+
+class QuestionAdmin(admin.ModelAdmin):
+  fieldsets = [
+      (None,               {'fields': ['question_text']}),
+      ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+  ]
+
+admin.site.register(Question, QuestionAdmin)
